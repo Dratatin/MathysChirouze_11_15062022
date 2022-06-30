b0d5dbb8b9634512ba67484180b48ae7395022f2
Dratatin <matchirouze@orange.fr>
2022-06-30 13:09:17 +0200

    creation of an api hook to avoid repetition

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,36 +1,30 @@
 import HomeIMG from '../assets/bannerHome.png'
 import Banner from '../components/Banner'
 import Card from '../components/Card'
-import { useEffect, useState } from 'react'
+import { useFetch } from '../services/api'
 
 const Home = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetch('./logements.json')
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, [])
-
-  return (
-    <div className="home">
-      <Banner imgBanner={HomeIMG} title={"Chez vous, partout et ailleurs"}/>
-      <section className="home__houses">
-        <ul className="home__houses__list">
-          {data.map((item) => (
-            <li key={item.id}>
-                <Card id={item.id} title={item.title} img={item.cover} />
-            </li>
-          ))}
-        </ul>
-      </section>
-    </div>
-  );
+  const { data, isLoading, error } = useFetch("../../logements.json")
+  if (error) {
+    return <span>Il y a eu un problème</span>
+  } else if (isLoading) {
+    return <div>Chargement...</div>
+  } else {
+    return (
+      <div className="home">
+        <Banner imgBanner={HomeIMG} title={"Chez vous, partout et ailleurs"}/>
+        <section className="home__houses">
+          <ul className="home__houses__list">
+            {data.map((item) => (
+              <li key={item.id}>
+                  <Card id={item.id} title={item.title} img={item.cover} />
+              </li>
+            ))}
+          </ul>
+        </section>
+      </div>
+    )
+  }
 }
 
 export default Home
--- src/pages/Housing.js
+++ src/pages/Housing.js
@@ -1,5 +1,5 @@
 import { useParams, Navigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useFetch } from '../services/api'
 import Slider from '../components/Slider';
 import Dropdown from '../components/Dropdown';
 import Tag from '../components/Tag';
@@ -8,76 +8,48 @@ import Stars from '../components/Starts';
 
 const Housing = () => {
   const { id } = useParams();
-  const [error, setError] = useState(null);
-  const [error404, setError404] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
-
-  // Remarque : le tableau vide de dépendances [] indique
-  // que useEffect ne s’exécutera qu’une fois, un peu comme
-  // componentDidMount()
-  useEffect(() => {
-    fetch("../../logements.json")
-      .then(response => response.json())
-      .then(
-        (result) => {
-          const logement = result.find(elem => elem.id === id);
-          setIsLoaded(true);
-          if (logement === undefined) {
-            setError404(true)
-          }
-          else {
-            setItems(logement);
-          }
-        },
-        // Remarque : il faut gérer les erreurs ici plutôt que dans
-        // un bloc catch() afin que nous n’avalions pas les exceptions
-        // dues à de véritables bugs dans les composants.
-        (error) => {
-          setIsLoaded(true);
-          setError(error);
-        }
-      )
-  }, [id])
+  const { data, isLoading, error } = useFetch("../../logements.json")
 
   if (error) {
-    return <div>Erreur : {error.message}</div>;
-  } else if (error404) {
-    return <Navigate  to='/error404' />
-  } else if (!isLoaded) {
-    return <div>Chargement...</div>;
+    return <span>Il y a eu un problème</span>
+  } else if (isLoading) {
+    return <div>Chargement...</div>
   } else {
+    const logement = data.find(elem => elem.id === id);
+
+    if (logement === undefined) return <Navigate  to='/error404' />
+
     return (
       <div className="housing">
-        <Slider tabimages={items.pictures} />
+        <Slider tabimages={logement.pictures} />
         <section className="housing__content">
           <div className="housing__content__heading">
             <div className="housing-details">
-              <h2>{items.title}</h2>
-              <h5 className="housing-details__subtitle" >{items.location}</h5>
+              <h2>{logement.title}</h2>
+              <h5 className="housing-details__subtitle" >{logement.location}</h5>
               <ul className="housing-details__tags">
-                {items.tags.map((tag, index) => (
+                {logement.tags.map((tag, index) => (
                   <Tag key={index} tag={tag}/>
                 ))}
               </ul>
             </div>
             <div className="housing-informations">
               <div className="housing-informations__host">
-                <img src={items.host.picture} alt={items.title}/>
-                <h5>{items.host.name}</h5>
+                <img src={logement.host.picture} alt={logement.title}/>
+                <h5>{logement.host.name}</h5>
               </div>
               <div className="housing-informations__stars">
-                  <Stars rating={items.rating}/>
+                  <Stars rating={logement.rating}/>
               </div>
             </div>
           </div>
           <div className="housing__content__description">
-            <Dropdown title="Description" content={items.description}/>
-            <Dropdown title="Equipement" content={items.equipments}/>
+            <Dropdown title="Description" content={logement.description}/>
+            <Dropdown title="Equipement" content={logement.equipments}/>
           </div>
         </section>
       </div>
-    );
+    )
   }
 }
 
